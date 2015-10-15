e6adc9f281795a5d225136dd43eef52bcb4ff0c2
hezedu <hezedu@hotmail.com>
2015-10-15 22:32:06 +0800

    2

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var sas = require('sas');
 #!/usr/bin/env node
 var path = require("path")
   , fs = require("fs")
   , args = process.argv.slice(1)
-
+  ,sas = require('sas');
 var arg, base;
 do arg = args.shift();
 while ( fs.realpathSync(arg) !== __filename
-  && !(base = path.basename(arg)).match(/^supervisor$|^supervisor.js$|^node-supervisor$/)
+  && !(base = path.basename(arg)).match(/^fuckwinfsdel$/)
 )
+console.log(args)
+//require.resolve("../sas/demo/removeTree.js")(args)
